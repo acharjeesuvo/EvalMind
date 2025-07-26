06b95089b1464b223eba9d7cdde0b0b77ddc9190
SUVOJIT ACHARJEE <124833499+acharjeesuvo@users.noreply.github.com>
2025-07-26 11:42:56 +0530

    Add files via upload

---
+++ test_supabase_Git.py
@@ -0,0 +1,24 @@
+import psycopg2
+
+
+SUPABASE_DB_CONFIG = {
+    "dbname": st.secrets["db"]["dbname"],
+    "user": st.secrets["db"]["user"],
+    "password": st.secrets["db"]["password"],
+    "host": st.secrets["db"]["host"],
+    "port": "5432"
+
+}
+
+def connection_check():
+    try:
+        conn = psycopg2.connect(**SUPABASE_DB_CONFIG)
+        cur = conn.cursor()
+        print("✅ Connected successfully into Supabase.")
+        cur.close()
+        conn.close()
+    except Exception as e:
+        print("❌ Connection failed:", e)
+
+connection_check()
+
